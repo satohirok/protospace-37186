b766a09bdc46e2eeb8fe673a059e1b51073907a4
satohirok <hiroki_g0421@yahoo.co.jp>
2021-11-29 19:23:10 +0900

    third commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   
-  before_action :authenticate_user!, except: [:index, :show, :new]  
+  before_action :authenticate_user!, except: [:index, :show ]  
+  
   def index
    @prototypes = Prototype.all
   end
