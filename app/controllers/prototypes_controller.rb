499283e155fac85c6e38419e656175ac5006158d
satohirok <hiroki_g0421@yahoo.co.jp>
2021-11-30 11:02:22 +0900

    third commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
-  
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show ]  
-  
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
    @prototypes = Prototype.all
   end
@@ -61,6 +62,10 @@ class PrototypesController < ApplicationController
 private
 def prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+end
+
+def set_prototype
+  @prototype = Prototype.find(params[:id])
 end
 
 def contributor_confirmation
